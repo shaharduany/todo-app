dbbc15909c7fd1be53105d7ceb7d682cf80887d7
Shahar Duany <shahar.duany@gmail.com>
2022-05-10 14:45:11 +0300

    Added user model

--- lib/db/models/User.ts
+++ lib/db/models/User.ts
@@ -1,1 +1,18 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		emailVerified: String,
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.models?.User || mongoose.model("user", UserSchema);
